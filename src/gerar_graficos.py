b6fb0407950b666e970305b293e479bf34f3d069
UaiJhonny <jhonny.louredo@estudante.ifgoiano.edu.br>
2026-09-13 01:14:22 +0000

    Adiciona grafico do experimento A

---
+++ src/gerar_graficos.py
@@ -0,0 +1,61 @@
+import os
+import matplotlib.pyplot as plt
+
+from mapa import CASA_DA_BARBIE
+from gerar_amigos import gerar_amigos
+from matriz_custos import calcular_matriz_custos
+from algoritmo_genetico import algoritmo_genetico
+
+
+SEED = 12345
+N = 8
+
+
+amigos = gerar_amigos(
+    N,
+    SEED
+)
+
+nomes, matriz = calcular_matriz_custos(
+    CASA_DA_BARBIE,
+    amigos
+)
+
+nomes_amigos = list(amigos.keys())
+
+
+melhor_cromossomo, melhor_custo, historico = algoritmo_genetico(
+    nomes_amigos,
+    nomes,
+    matriz,
+    tamanho_populacao=50,
+    geracoes=300,
+    taxa_crossover=0.8,
+    taxa_mutacao=0.03,
+    tamanho_elite=2
+)
+
+
+os.makedirs("resultados", exist_ok=True)
+
+
+plt.figure(figsize=(10, 5))
+
+plt.plot(
+    range(1, len(historico) + 1),
+    historico
+)
+
+plt.xlabel("Geração")
+plt.ylabel("Melhor custo")
+plt.title("Convergência do Algoritmo Genético - Experimento A")
+
+plt.grid()
+
+plt.savefig(
+    "resultados/experimento_A.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+
+plt.show()
